e10dff102552bf98bf4c93e8c2221da870ca8869
divy-14 <divymohanrai@gmail.com>
2021-03-17 16:14:31 +0530

    added the API view to handle booking requests

--- CarApi/views.py
+++ CarApi/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.views import APIView
-from .serializers import CarSerializer, UserSerializer
-from .models import Car, NewUser
+from .serializers import CarSerializer, UserSerializer, BookCarSerializer
+from .models import Car, NewUser, BookedCars
 from rest_framework import status, permissions
 from rest_framework.response import Response
+from datetime import datetime
+from dateutil import parser
 
 
 class CarApi(APIView):
@@ -85,3 +87,114 @@ class CalculateCost(APIView):
             return Response(
                 {'Bad Request try again, the number plate may be wrong'},
                 status=status.HTTP_404_NOT_FOUND)
+
+
+class BookCar(APIView):
+    '''
+    Book a car for a given duration
+    '''
+
+    def post(self, request, format=None):
+        booked_cars = BookedCars.objects.all()
+        cars = Car.objects.all()
+        users = NewUser.objects.all()
+        mobileNum = request.data['userid']
+        to_ = request.data["toDate"]
+        from_ = request.data["fromDate"]
+        carNumber = request.data["carLicenseNumber"]
+
+        to_ = datetime.strptime(to_, '%Y-%m-%d %H:%M:%S')
+        from_ = datetime.strptime(from_, '%Y-%m-%d %H:%M:%S')
+
+        if to_ < from_:
+            return Response(
+                {'Not possible end date has to be greater than start date'},
+                status=status.HTTP_403_FORBIDDEN)
+
+        # user not found in the user database
+        FoundUser = False
+        for user in users:
+            if int(user.userMobile) == int(mobileNum):
+                FoundUser = True
+
+        if FoundUser == False:
+            return Response(
+                {'User with given mobile number/id not Found'},
+                status=status.HTTP_404_NOT_FOUND)
+
+        # car with the given numberplate not in the car database
+        FoundPlate = False
+        for obj in cars:
+            # comparing strings imp..
+            if str(obj.carLicenseNumber) == str(carNumber):
+                FoundPlate = True
+
+        if FoundPlate == False:
+            return Response(
+                {'Car with given number plate not Found'},
+                status=status.HTTP_404_NOT_FOUND)
+
+        # different conditions
+        else:
+            time_intervals = []
+            availableAfter = None  # we will find what is the time after which a car is available
+            for obj in booked_cars:
+                if str(obj.carLicenseNumber) == str(carNumber):
+                    time_intervals.append(
+                        (obj.fromDate.replace(tzinfo=None), obj.toDate.replace(tzinfo=None)))
+                    if availableAfter is None:
+                        availableAfter = obj.toDate
+                    else:
+                        availableAfter = max(availableAfter, obj.toDate)
+
+            time_intervals.sort()
+
+            # if the car is not yet booked by anyone
+            if availableAfter == None:
+                serializer = BookCarSerializer(data=request.data)
+                permission_classes = [permissions.AllowAny]
+
+                if serializer.is_valid(raise_exception=True):
+                    serializer.save()
+                    return Response(serializer.data)
+
+            # if the car in not available in the given interval
+            for time in time_intervals:
+                if from_ < time[1] and from_ >= time[0]:
+                    return Response(
+                        {'Car not available:': carNumber,
+                         'Car Booked for:': time},
+                        status=status.HTTP_403_FORBIDDEN)
+
+                if from_ <= time[0] and to_ >= time[1]:
+                    return Response(
+                        {'Car not available:': carNumber,
+                         'Car Booked for:': time},
+                        status=status.HTTP_403_FORBIDDEN)
+
+                if to_ > time[0] and to_ <= time[1]:
+                    return Response(
+                        {'Car not available:': carNumber,
+                         'Car Booked for:': time},
+                        status=status.HTTP_403_FORBIDDEN)
+
+            # else if the car is available we book it
+            serializer = BookCarSerializer(data=request.data)
+            permission_classes = [permissions.AllowAny]
+
+            try:
+                if serializer.is_valid(raise_exception=True):
+                    serializer.save()
+                    return Response(serializer.data)
+            except:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+'''
+3 cases:
+car booked for [(27, 29)], car booking request for examples:
+1. [(27, 29)]  -> incoming (24, 30) return False
+2. [(27, 29)]  -> incoming (24, 28) return False
+3. [(27, 29)]  -> incoming (28, 31) return False
+4. [(24, 30)]  -> incoming (27, 29) return False
+'''
