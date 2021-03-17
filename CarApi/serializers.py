d72ef4974ead35e6c1ac04e5f5f87d44d82ef4d2
divy-14 <divymohanrai@gmail.com>
2021-03-17 16:13:33 +0530

    added the Serializer for Booking a Car

--- CarApi/serializers.py
+++ CarApi/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Car, NewUser
+from .models import Car, NewUser, BookedCars
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -14,3 +14,9 @@ class CarSerializer(serializers.ModelSerializer):
         model = Car
         fields = ["carLicenseNumber", "Manufacturer",
                   "Model", "base_price", "pph", "security_deposit"]
+
+
+class BookCarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookedCars
+        fields = ["userid", "toDate", "fromDate", "carLicenseNumber"]
