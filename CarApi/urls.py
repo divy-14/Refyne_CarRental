822a6c28f51f5e4af95f322b36bab34eec9d69f4
divy-14 <divymohanrai@gmail.com>
2021-03-18 03:37:29 +0530

    added swagger implementation for the API

--- CarApi/urls.py
+++ CarApi/urls.py
@@ -1,8 +1,26 @@
 from django.urls import path, include
 from . import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Refyne CarRental API",
+        default_version='v1',
+        description="An API for managing CAR Rentals",
+        contact=openapi.Contact(email="divymohanrai@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 # TODO: add url to figure out which cars are available for a given duration
 urlpatterns = [
+    path('', schema_view.with_ui('swagger',
+                                 cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+                                       cache_timeout=0), name='schema-redoc'),
     path('user/', views.UserApi.as_view(), name="add-user"),
     path('cars/', views.CarApi.as_view(), name="add-car"),
     path('calculate-price/', views.CalculateCost.as_view(), name="price"),
--- CarRentalApi/settings.py
+++ CarRentalApi/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'CarApi',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
