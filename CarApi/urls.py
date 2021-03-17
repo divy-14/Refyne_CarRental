6b1300a1b22071b8954c9a923a06ca2ca6153f6c
divy-14 <divymohanrai@gmail.com>
2021-03-17 16:15:07 +0530

    added the path to handle incoming booking requests

--- CarApi/urls.py
+++ CarApi/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('user/', views.UserApi.as_view(), name="add-user"),
     path('cars/', views.CarApi.as_view(), name="add-car"),
-    path('calculate-price/', views.CalculateCost.as_view(), name="price")
+    path('calculate-price/', views.CalculateCost.as_view(), name="price"),
+    path('car/book/', views.BookCar.as_view(), name="book-car"),
 
 ]
+# car/book
